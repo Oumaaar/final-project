1b420d603cf873ad53e550352643f42c49659d65
Oumaaar <oumaarbs@gmail.com>
2022-03-18 18:40:14 +0100

    changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://memo-app33.herokuapp.com/posts" }); 
+const API = axios.create({ baseURL: "http://localhost:5000" }); 
 
 API.interceptors.request.use((req) => {
 	if (localStorage.getItem("profile")) {
